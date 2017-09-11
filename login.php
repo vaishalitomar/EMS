99bdc39a993b6398f81ecc3f566845b1ee121d5b
nidhi1999 <31519278+nidhi1999@users.noreply.github.com>
2017-09-11 16:48:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
- 
+ include('db_connection.php');
 $error=''; // Variable To Store Error Message
 if (isset($_POST['submit'])) 
 {
@@ -11,21 +11,12 @@ else
 {
 // Define $username and $password
 $d=$_POST['desn'];
-$username=test_input($_POST['username']);
-$password=test_input($_POST['password']);
+$username=mysqli_real_escape_string($connection, $_POST['username']);
+$password=mysqli_real_escape_string($connection, $_POST['password']);
 
 }
 
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysqli_connect("localhost", "root","1234");
- if(mysqli_connect_errno())
- {
-  echo "failed to connect to mysql:" . mysqli_connect_errno();
- }
-
-
-// Selecting Database 
-$db = mysqli_select_db($connection, "ems1");
 
 if($d=="0")
 {
@@ -53,9 +44,5 @@ $error = "Username or Password is invalid";
 }
 mysqli_close($connection); // Closing Connection
 }
-function test_input($data) 
-{
-  $data = mysqli_real_escape_string($connection, $_POST['data']);
-  return $data;
-}
+
 ?>
