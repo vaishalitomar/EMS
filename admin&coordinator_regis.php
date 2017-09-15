d13a47106119b47a786471873ff9c3a1c9f40c42
nidhi1999 <31519278+nidhi1999@users.noreply.github.com>
2017-09-16 00:09:50 +0530

    admin&cod_regis.php with session

--- admin&coordinator_regis.php
+++ admin&coordinator_regis.php
@@ -2,6 +2,8 @@
  include('db_connection.php'); 
  $error=""; // Variable To Store Error Message 
 session_start();
+if(isset($_SESSION["name"])
+   {
  if (isset($_POST['submit']))  
  { 
  if (empty($_POST['username']) || empty($_POST['password'])) 
@@ -15,7 +17,8 @@ session_start();
  $d=$_POST['desn']; 
 $username=mysqli_real_escape_string($connection, $_POST['username']); 
  $password=mysqli_real_escape_string($connection, $_POST['password']); 
-  }
+ }
+ }
   if($d=="0") 
  { 
  $query = mysqli_query($connection,"INSERT INTO login(username, password, designation) VALUES('$username', '$password', '$d')"); 
@@ -35,10 +38,14 @@ if(!$query)
 echo "registered as a coordinator";
 }
 
-  
+  if(isset($_POST["logout"])
+     {
+      session_destroy();
+header("location:enter.php");
+     }
  
  mysqli_close($connection);
 }
 
-
+   
  ?> 
