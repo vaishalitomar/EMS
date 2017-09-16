729ab836cde2680b980d2315a63f736aa407bb23
nidhi1999 <nidfhi@gmail.com>
2017-09-16 23:15:58 +0530

    integrated login page with html

--- login.php
+++ login.php
@@ -1,49 +1,48 @@
 <?php
  include('db_connection.php');
 $error=''; // Variable To Store Error Message
+session_start();
+
 if (isset($_POST['submit'])) 
 {
-if (empty($_POST['username']) || empty($_POST['password']))
+if (empty($_POST['uname']) || empty($_POST['psw']))
  {
 $error = "Username or Password is invalid";
+
 }
 else
 {
 // Define $username and $password
-$d=$_POST['desn'];
-$username=mysqli_real_escape_string($connection, $_POST['username']);
-$password=mysqli_real_escape_string($connection, $_POST['password']);
-
+$designation=$_POST['person'];
+$username=mysqli_real_escape_string($connection, $_POST['uname']);
+$password=mysqli_real_escape_string($connection, $_POST['psw']);
 }
-
+$_SESSION["name"]=$username;
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-if($d=="2")
+if($designation=="2")
 {
- $query = mysqli_query($connection,"select * from login where password='$password'AND username='$username' AND designation='2'");
+ $query = mysqli_query($connection,"SELECT * from login where password='$password'AND username='$username' AND designation='2'");
 $rows = mysqli_num_rows($query);
 if ($rows == 1 ) {
  
-
 header("location: registration1.php"); // Redirecting To Other Page
 }
 }
-if($d=="0")
+if($designation=="0")
 {
-$query = mysqli_query($connection,"select * from login where password='$password'AND username='$username' AND designation='0'");
+$query = mysqli_query($connection,"SELECT * from login where password='$password'AND username='$username' AND designation='0'");
 $rows = mysqli_num_rows($query);
 if ($rows == 1 ) {
  
-
-header("location: admin.html"); // Redirecting To Other Page
+header("location: society.php"); // Redirecting To Other Page
 }
 }
-if($d=="1")
+if($designation=="1")
 {
-$query = mysqli_query($connection,"select * from login where password='$password' AND username='$username' AND designation='1'");
+$query = mysqli_query($connection,"SELECT * from login where password='$password' AND username='$username' AND designation='1'");
 $rows = mysqli_num_rows($query);
 if ($rows == 1 ) {
  
-
 header("location: coordinator.php"); 	
 }
 }
@@ -53,5 +52,4 @@ $error = "Username or Password is invalid";
 }
 mysqli_close($connection); // Closing Connection
 }
-
 ?>
