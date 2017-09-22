245fc3291ee9b986ec539420feda9905ae256af8
vaishalitomar <vicky.11081997@gmail.com>
2017-09-22 14:49:58 +0530

    display student detail in table

--- view_student_detail.php
+++ view_student_detail.php
@@ -1,7 +1,7 @@
 <?php
 include('connect_data.php');
 if(!empty($_SESSION['username']))
-{if(($_SESSION['position']=='A')||!empty($_SESSION['society_id']))
+{if(($_SESSION['position']=='A')||(!empty($_SESSION['society_id'])))
 	{if(isset($_POST['event']))
 {$event=mysqli_real_escape_string($link,$_POST['event']);
 	$event=strtolower($event);
@@ -9,23 +9,38 @@ if(!empty($_SESSION['username']))
 	{$society_name=$_SESSION['society_name'];}
 if(!empty($event))
 {if($_SESSION['position']=='A')
-	{$query="SELECT * FROM `registrations` WHERE`event_name`='$event'";}
+	{$query="SELECT * FROM `registrations` WHERE `event_name`='$event'";}
 else
 {$query="SELECT * FROM `registrations` WHERE(`event_name`='$event' AND `society_name`='$society_name')";}
 $query_run=mysqli_query($link,$query);
 if($query_run==true)
-{if(mysqli_num_rows($query_run)>=1)
-	{$count=1;while($row=mysqli_fetch_assoc($query_run))
-{	echo $count.$row['name'].' '.$row['student_no'].' '.$row['branch'].' '.$row['year'].' '.$row['email'].' '.$row['contact_no'].' '.$row['hostler'].'<br>';
+{echo "<table border ='1'>
+<tr>
+<th>STUDENT NAME</th>
+<th>STUDENT NO</th>
+<th>BRANCH</th>
+<th>YEAR</th>
+<th>CONTACT NO</th>
+<th>EMAIL </th>
+</tr>";
+//if(mysqli_num_rows($query_run)>=1)
+$count=1;
+while($row=mysqli_fetch_assoc($query_run))
+{	echo "<tr>"; 
+	echo "<td>".$row['name']."</td>";
+	echo "<td>".$row['student_no']."</td>";
+	echo "<td>".$row['branch']."</td>";
+	echo "<td>".$row['year']."</td>";
+	echo "<td>".$row['contact_no']."</td>";
+	echo "<td>".$row['email']."</td>";
+//echo $count.$row['name'].' '.$row['student_no'].' '.$row['branch'].' '.$row['year'].' '.$row['email'].' '.$row['contact_no'].' '.$row['hostler'].'<br>';
 $count=$count+1;
 }
 }
 else
 { echo 'no student regisrter for this';}
 }
-else
-	{echo 'error'.mysqli_error($link);}
-}
+
 else
 {echo 'plz enter a valid event name';}
 }
