afa20a8dbd6bda4e5bcc6d76c7206af5445444cc
vaishalitomar <vicky.11081997@gmail.com>
2017-09-22 10:50:29 +0530

    modification in content delete (dropdown list)

--- delete_uploaded_file.php
+++ delete_uploaded_file.php
@@ -2,16 +2,17 @@
 include ('connect_data.php');
 if(!empty($_SESSION['username']))
 {if(!empty($_SESSION['society_id']))
-{if(isset($_POST['delete_file']))
-{$delete_file=mysqli_real_escape_string($link,$_POST['delete_file']);
-if(!empty($delete_file))
+{if(isset($_POST['file_name']))
+{$file_name=mysqli_real_escape_string($link,$_POST['file_name']);
+if(!empty($file_name))
 {$society_name=$_SESSION['society_name'];
-	if(unlink("uploaded/$society_name/".$_POST['delete_file']))
+	var_dump($society_name);
+	if(unlink("uploaded/".$society_name."/".$_POST['file_name']))
 		{echo 'file has been deleted'.'<br>';}
 else{
 	echo 'file deleteion fail enter a valid name ';
 }
-$query="DELETE  FROM `upload` WHERE `file_name`='$delete_file'";
+$query="DELETE  FROM `upload` WHERE `file_name`='$file_name'";
 $query_run=mysqli_query($link,$query);
              if($query_run==false)
 
@@ -24,11 +25,11 @@ $query_run=mysqli_query($link,$query);
 $query="SELECT * FROM `upload` WHERE `society_id`='$society_id'";
 $query_run=mysqli_query($link,$query);
 ?>
-		<form method="post" action=" delete_uploaded_file">
+		<form method="post" action=" delete_uploaded_file.php">
  
  <label >file name</label>
  
- <select name="file_names">
+ <select name="file_name">
  
   <?php
 if($query_run==true)
@@ -39,9 +40,10 @@ if($query_run==true)
   if(mysqli_num_rows($query_run)>=1){
 	while($row=mysqli_fetch_assoc($query_run))
 	{ ?><option>
-<option value="1"> <?php echo $count.' '.$row['file_name']?>
+<option > 
+	<?php echo $row['file_name']?>
+</option> 
 
-</option> ?>
 <?php
 	     $count=$count+1;
 	 }
@@ -51,11 +53,14 @@ if($query_run==true)
 	{
 		echo 'no file';
 	}
-}
+}}?><input type ="submit" value="DELETE">
+</form>
+<?php
 //else
 //{ echo 'no file ';}
-}else
-{echo 'error'.mysqli_error($link);}
+//else
+//{echo 'error'.mysqli_error($link);}
+//}
 }
 else
 	{ header('location:go_to.php');}
@@ -64,7 +69,7 @@ else
 {header('location:login.php');}
 
 ?>
-<form action="delete_uploaded_file.php" method="POST">
-<!--<input type ="text" name ="delete_file" maxlength="30">--> 
+<!--<form action="delete_uploaded_file.php" method="POST">
+<input type ="text" name ="delete_file" maxlength="30"> 
 <input type ="submit" value="DELETE">
 </form>
