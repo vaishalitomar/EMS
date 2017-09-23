c388ff24e5ce01d229efa3844224818fddc3d5a8
vaishalitomar <vicky.11081997@gmail.com>
2017-09-23 22:57:47 +0530

    aborting an event by entering event name with html

--- abort2.php
+++ abort2.php
@@ -1,49 +1,86 @@
 <?php
-include('connect_data.php');
+include('db_connection.php');
 if(!empty($_SESSION['username']))
-{if(($_SESSION['position']=='A')||!empty($_SESSION['society_id']))
-	{if(isset($_POST['society_name'],$_POST['event_name']))
- {$society_name=$_POST['society_name'];$event_name=$_POST['event_name'];
-if(!empty($society_name)&&!empty($event_name))
-{$society_name=mysqli_real_escape_string($link,$society_name);$society_name=strtolower($society_name);$society_id=mysqli_real_escape_string($link,$_SESSION['society_id']);
-if($society_name==$_SESSION['society_name'])
-{$query=" DELETE FROM `event` WHERE (`event_name`='$event_name' AND `society_id`='$society_id')";
-
-	  $query_run=mysqli_query($link,$query);
-		if($query_run==true)
-		{$r=mysqli_num_rows($query_run);
-			if($r==1)
-			{header('location:abort1.php');
-				
-
-                     }
-					 else
+{
+	if(($_SESSION['position']=='A')||!empty($_SESSION['society_id']))
+	{ 
+		if(isset($_POST['event_name']))
+       {
+       	$event_name=$_POST['event_name'];
+          if(!empty($event_name))
+             $society_id=mysqli_real_escape_string($connection,$_SESSION['society_id']);
+	         if(isset($_POST['yes']))
+	           {
+                { $query=" DELETE FROM `event` WHERE (`event_name`='$event_name')";
+                    $query_run=mysqli_query($connection,$query);
+		          if($query_run==true)
+		         {
+			      {
+			      	header('location:abort1.php');
+			      }
+                 }
+			    else
 					 { echo 'invalid event name or no such event';}
-			}
-		else
-		{echo 'error'.mysqli_error($link);}
-		
+			    }
+	           }
+             else
+                { echo 'invalid entry dont try to delete of another society';}
+
+        }
+        else 
+          {echo 'plz fill all the field';}
+    }
+      else
+	    { header('location:go_to.php');}
 }
-else
-{ echo 'invalid entry dont try to delete of another society';
-}
-}else 
-{echo 'plz fill all the field';}
- }
-}else
-	
-	{ header('location:go_to.php');}
-}
-else
-{header('location:login.php');}
+
+  else
+     {header('location:login.php');}
 
 
 ?>
-	<em><br> BE CAREFUL DELETED DATA CANNOT BE RETRIVED</em>			
-<form action="abort2.php" method="POST">
-society name:<br>
-<input type="text" name="society_name">
-<br><br>event name:<br>
-<input type="text" name ="event_name"><br><br>
-<input type="submit" value="submit" >
-</form>
+	
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>EMS-AKGEC</title>
+    <link rel="stylesheet" type="text/css" href="css/emscss.css">
+
+<style>
+body{
+       background: url("images/coodpgbg.jpg") no-repeat center fixed; 
+       background-size: cover;
+     }
+</style>
+
+</head>
+
+<body>
+<div class="header">
+<a href="http://www.akgec.in/" target="_blank"><img style="float: left;" src="images/akgeclogo.png">  </a>
+    <h1>Event Management system</h1>
+    </div>
+<div class="welcome">Welcome Coordinator</div>
+<div class="row">
+
+<div class="register">
+  <div class="aside">
+    <h1>Do you Want to Abort the event ?</h1>
+      <form action="abort2.php" method="post">
+        event name:<br>
+            <input type="text" name ="event_name"><br><br>
+               <button name="yes" class="button">Yes</button>
+               <button name = "no" class="button">No</button>
+      </form>
+     <br>
+  </div>
+</div>
+</div>
+
+<div class="footer">
+  <p>Thank You</p>
+</div>
+
+</body>
+</html>
