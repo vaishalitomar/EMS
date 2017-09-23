c776d84bade9ef5f320ab2a120e5b900c46b1935
vaishalitomar <vicky.11081997@gmail.com>
2017-09-23 22:58:59 +0530

    to abort an event

--- abort1.php
+++ abort1.php
@@ -1,19 +1,17 @@
+
 <?php
-<?php
-include('connection.php');
+include('db_connection.php');
 if(!empty($_SESSION['username']))
-{if(!empty($_SESSION['society_id']))
-if($c==1)
-{echo ' you have suceesfully abort the event';
-}
-else
-{header('location:go_to.php');
-}
-else
-	
-	{ header('location:go_to.php');}
-}
-else
-{header('location:login.php');}
+   {
+     if(!empty($_SESSION['society_id']))
+     {
+       echo ' you have suceesfully abort the event';
+     }
+     else
+      {header('location:go_to.php');}
+   }
+
+     else
+      {header('location:login.php');}
 
 ?>
