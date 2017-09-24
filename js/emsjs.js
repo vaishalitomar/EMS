e36049961503e32d784299d00064971faaeb9ee5
parammittalgr8@gmail.com <parammittalgr8@gmail.com>
2017-09-24 20:49:11 +0530

    BOTTOM TO TOP GOING BUTTON MODIFIED

--- js/emsjs.js
+++ js/emsjs.js
@@ -1,3 +1,5 @@
+//=================TYPEWRITER=============
+
 var i = 0;
 var txt = 'Upcoming Events...';
 var speed = 200;
@@ -10,29 +12,9 @@ function typeWriter() {
   }
 }
 
-//
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
 
-function off() {
-    document.getElementById("overlay").style.display = "none";
-}
-window.onscroll = function() {scrollFunction()};
+//==================MODAL===============
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
 
 // Get the modal
 var modal = document.getElementById('myModal');
@@ -59,22 +41,34 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+
+// ========= Scroll to Top =============== 
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
     } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        document.getElementById("myBtn").style.display = "none";
     }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 
-/*SLIDER*/
+//================= SLIDER ==========================
 
 (function() {
   
