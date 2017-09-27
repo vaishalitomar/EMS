b1adccb7ca78c1b18310596ddafdac5e798af2cd
vaishalitomar <vicky.11081997@gmail.com>
2017-09-27 14:19:09 +0530

    view student detail by coordinator(modification in query)

--- view_student_detail.php
+++ view_student_detail.php
@@ -1,20 +1,30 @@
 <?php
-include('connect_data.php');
-if(!empty($_SESSION['username']))
-{if(($_SESSION['position']=='A')||(!empty($_SESSION['society_id'])))
-	{if(isset($_POST['event']))
-{$event=mysqli_real_escape_string($link,$_POST['event']);
-	$event=strtolower($event);
-	if(!empty($_SESSION['society_name']))
-	{$society_name=$_SESSION['society_name'];}
-if(!empty($event))
-{if($_SESSION['position']=='A')
-	{$query="SELECT * FROM `registrations` WHERE `event_name`='$event'";}
-else
-{$query="SELECT * FROM `registrations` WHERE(`event_name`='$event' AND `society_name`='$society_name')";}
-$query_run=mysqli_query($link,$query);
+include('db_connection.php');
+session_start();
+if(isset($_SESSION["name"]))
+
+	{
+		if(isset($_POST['submit']))
+		{
+			
+		
+
+	$event=mysqli_real_escape_string($connection,$_POST['event']);
+	
+	
+            {
+
+                      $query1=mysqli_query($connection, "SELECT * FROM `event` WHERE `event_name`='$event'");
+                      $rows=mysqli_num_rows($query1);
+                      if($rows>0)
+        {
+                        $row1=mysqli_fetch_array($query1);
+                        $event_id=$row1['event_id'];
+
+	$query="SELECT * FROM `registrations` WHERE(`event_id`='$event_id')";
+$query_run=mysqli_query($connection,$query);
 if($query_run==true)
-{echo "<table border ='1'>
+{echo "<table border ='3'>
 <tr>
 <th>STUDENT NAME</th>
 <th>STUDENT NO</th>
@@ -37,23 +47,115 @@ while($row=mysqli_fetch_assoc($query_run))
 $count=$count+1;
 }
 }
+}
 else
 { echo 'no student regisrter for this';}
 }
 
-else
-{echo 'plz enter a valid event name';}
 }
-	}
-	else
-	{ header('location:go_to.php');}
 }
-else
-{header('location:login.php');}
+
+
+else{
+	echo 'please submit the request';
+}
+	
+	
+
 
 ?>
-<form action="view_student_detail.php" method="POST">
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>EMS-AKGEC</title>
+    <link rel="stylesheet" type="text/css" href="css/emscss.css">
+
+<style>
+body{
+       background: url("images/coodpgbg.jpg") no-repeat center fixed; 
+       background-size: cover;
+     }
+</style>
+
+</head>
+
+<body>
+
+<script src="js/emsjs.js">
+</script>
+
+<button onclick="topFunction()" id="myBtn" title="Go to top">&nbsp;^&nbsp;</button>
+
+<div class="header">
+<a href="http://www.akgec.in/" target="_blank"><img style="float: left;" src="images/akgeclogo.png"></a>
+  <a href="coordinator1.html"><img class='imgpop' style="float: right;" src="images/lo.png"></a>
+  <a href="index.html" title="HOME" ><img class='imgpop' style="float: right;" src="images/home2.png"></a>
+  <h1 style="text-align: center;" >EVENT MANAGEMENT SYSTEM</h1>
+</div>
+<div class="welcome">Students who have registered</div>
+<div style="overflow-x:auto;opacity: 0.8;padding: 20px;">
+<form action="view_student_detail.php" method="post">
 event name<br><br>
 <input type="text" name="event" >
-<input type="submit">
+<input type="submit" name = "submit" value = "submit" >
 </form>
+  <!--<table>
+    <tr>
+      <th>Student number</th>
+      <th>Student Name</th>
+      <th>Gender</th>
+      <th>Email</th>
+      <th>Mobile No.</th>
+      <th>Class</th>
+      <th>Hostler</th>
+      <th>Document uploaded</th>
+    </tr>
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td> 
+      <button type="submit" class="button2">View</button>
+      </td>
+    </tr>
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td> 
+      <button type="submit" class="button2">View</button>
+      </td>
+    </tr>
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td> 
+      <button type="submit" class="button2">View</button>
+      </td>
+    </tr>
+    
+  </table> -->
+</div>
+</div>
+
+<div class="footer">
+  <p>Thank You</p>
+</div>
+
+</body>
+</html>
