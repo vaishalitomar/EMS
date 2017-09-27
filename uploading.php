d6ebebb94084dc2b7a0851434abd077e77600996
vaishalitomar <vicky.11081997@gmail.com>
2017-09-27 14:20:22 +0530

    uploading of event poster in front page

--- uploading.php
+++ uploading.php
@@ -1,43 +1,58 @@
 <?php
 include('db_connection.php');
-if(!empty($_SESSION['username']))
+session_start();
+if(isset($_SESSION['name']))
    {
-    if(!empty($_SESSION['society_id']))
-	    {
-          if(isset($_FILES['file']['name'])&&isset($_POST['rename']))
+          if(isset($_POST['submit']))
          {
-          $name=$_FILES['file']['name'];$rename=$_POST['rename'];$society_id=$_SESSION['society_id'];$society_name=$_SESSION['society_name'];
+          $name=$_FILES['file']['name'];
+          $rename=$_POST['rename'];
                if(!empty($name)&&!empty($rename))
              {
               $offset=0; 
                    while($count=strpos($name,'.',$offset))
                 {
-                 $offset=$count+1;}
+                 $offset=$count+1;
+                }
                 $ext= strtoupper(substr($name,$offset));
 
-                     if(($ext=='PDF')||($ext=='TXT')||($ext=='PNG')||($ext=='JPEG')||($ext=='JPG'))
+                     if(($ext=='PNG')||($ext=='JPEG')||($ext=='JPG'))
                  {
                    $offset=0; 
                         while($count=strpos($rename,'.',$offset))
                   {
                    $offset=$count+1;}
                   $ext= strtoupper(substr($rename,$offset));
-                          if(($ext=='PDF')||($ext=='TXT')||($ext=='PNG')||($ext=='JPEG')||($ext=='JPG'))
+                          if(($ext=='PNG')||($ext=='JPEG')||($ext=='JPG'))
     
                   	{  
+                      $cod_name=$_SESSION["name"];
+                      $query1=mysqli_query($connection, "SELECT * FROM coordinator WHERE `coordiantor_name`='$cod_name'");
+                      $rows=mysqli_num_rows($query1);
+                      if($rows>0)
+                      {
+                        $row=mysqli_fetch_array($query1);
+                        $society_id=$row['society_id'];
                       $query="INSERT INTO `upload`(`file_name`,`society_id`) VALUES('$rename','$society_id')";
                       $query_run=mysqli_query($connection,$query);
                      if($query_run==true)
                        {
                            $from=$_FILES['file']['tmp_name'];
-                           $to='c:\xampp\htdocs\EMS\uploaded/';
-                          if(move_uploaded_file($from,$to.$society_name.'/'.$rename))
-                          {echo 'file has been uploaded';}
-                          else{echo 'upload in directory failed';}			
-                           echo 'data has been entered succesfully'.'<br>';
+                           $to='c:\xampp\htdocs\EMS2\uploaded/';
+                          if(move_uploaded_file($from,$to.$rename))
+                          { 
+                            header('location:success.php');
+                          }
+                          else
+                            {
+                              echo 'upload in directory failed';
+                        }			
+                           
                        }
                      else
-                          {echo 'error'.mysqli_error($connection);}
+                          {
+                            echo 'error'.mysqli_error($connection);
+                          }
       
 			
 		             		}
@@ -53,11 +68,7 @@ if(!empty($_SESSION['username']))
               {echo 'plz fill all the field';}
 	       }
 	}
-	     else
-	        { header('location:go_to.php');}
-}
-else
-{header('location:login.php');}
+	     
 
 ?>
 
@@ -100,8 +111,8 @@ body{
    
    <div class="register">
      <div class="aside3">
-       <h2 style="color: blue;text-align: center;"><b>Upload File</b></h2><br>
-         <form action="uploading.php" method = "POST" enctype="multipart/form-data">
+       <h2 style="color: blue;text-align: center;"><b>Upload File (400*300)</b></h2><br>
+         <form action="uploading.php" method = "post" enctype="multipart/form-data">
 
             <label style="text-align: left;"><b>Upload File:</b></label><br><br>
             <input type="file" name="file" id="fileToUpload"><br><br>
@@ -109,7 +120,8 @@ body{
             <input type="text" name="rename" id="textdeco" placeholder="Enter file name" required="required"/>
             <br><br> 
             
-            <input style="width: 20%" type="submit" value="submit">
+            
+            <input style="width: 20%" type="submit" name = "submit" value="submit">
             <br><br>       
          </form>
   </div>
@@ -124,3 +136,6 @@ body{
 
 </body>
 </html>
+<?php
+}
+?>
