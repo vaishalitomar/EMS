6a23dd080323f3c72868324a3c33c6dfa5b6c889
vaishalitomar <vicky.11081997@gmail.com>
2017-10-01 10:16:20 +0530

    to abort event(event name in dropdown)

--- abort2.php
+++ abort2.php
@@ -3,24 +3,24 @@ include('db_connection.php');
 session_start();
 if(isset($_SESSION['name']))
 {
-	
-	 
-		if(isset($_POST['event_name']))
+  
+   
+    if(isset($_POST['event']))
        {
-       	$event_name=$_POST['event_name'];
-          if(!empty($event_name))
-           { 
-	         if(isset($_POST['yes']))
-	           
+       
+           if(isset($_POST['submit']))
+             
                 { 
+                     
+                  $event_name=$_POST['event'];
 
                                 
                    $query1=mysqli_query($connection, "SELECT * FROM `event` WHERE `event_name`='$event_name'");
                       $rows=mysqli_num_rows($query1);
                       if($rows>0)
               {
-                        $row=mysqli_fetch_array($query1);
-                        $event_id=$row['event_id'];
+                        $row1=mysqli_fetch_array($query1);
+                        $event_id=$row1['event_id'];
 
                 }
 
@@ -31,30 +31,25 @@ if(isset($_SESSION['name']))
 
              
 
-		          if((($query_run)&&($query_run2))==true)
-		         {
-			      {
+              if((($query_run)&&($query_run2))==true)
+             {
+            {
               
-			      	header('location:success.php');
-			      }
+              header('location:success.php');
+            }
                  }
-			    else
-					 { echo 'invalid event name or no such event';}
-			    }
-	           }
+          else
+           { echo 'invalid event name or no such event';}
+          }
+             }
 
-             else
-                { echo 'invalid entry dont try to delete of another society event';}
            
-        }
+           
         
-    }
+        
+    
      
 
-
-  else
-     //{header('location:index.php');}
-{echo "noooo";}
 
 ?>
 	
@@ -63,6 +58,7 @@ if(isset($_SESSION['name']))
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>EMS-AKGEC</title>
+    <link rel="icon" href="images/logoic.ico">
     <link rel="stylesheet" type="text/css" href="css/emscss.css">
 
 <style>
@@ -75,9 +71,15 @@ body{
 </head>
 
 <body>
+<script src="validation.js"></script>
 <div class="header">
-<a href="http://www.akgec.in/" target="_blank"><img style="float: left;" src="images/akgeclogo.png">  </a>
-    <h1>Event Management system</h1>
+<a href="http://www.akgec.in/" title="AKGEC WEBSITE" target="_blank"><img style="float: left;" src="images/akgeclogo.png"></a>
+  <a href="logout.php" title="LOGOUT"><img class='imgpop' style="float: right;" src="images/lo.png"></a>
+  <a href = "coordinator_frontpage.php" title="HOME" ><img class='imgpop' style="float: right;" src="images/home2.png"></a>
+  <div style="text-align: center;">
+  <img src="images/headerlogo.png">
+  <h1 style="font-size: 40px; color: #00ABDC">EVENT MANAGEMENT SYSTEM</h1>
+  </div>>
     </div>
 <div class="welcome">Welcome Coordinator</div>
 <div class="row">
@@ -85,11 +87,61 @@ body{
 <div class="register">
   <div class="aside">
     <h1>Do you Want to Abort the event ?</h1>
-      <form action="abort2.php" method="post">
-        event name:<br>
-            <input type="text" name ="event_name"><br><br>
-               <button name="yes" class="button">Yes</button>
-               <button name = "no" class="button">No</button>
+      <form id="abort" action="abort2.php" method="post" onsubmit="return abortonsubmit()">
+      <p id="name"></p>
+      <?php
+
+  $cod_name=$_SESSION["name"];
+                      $query1=mysqli_query($connection, "SELECT * FROM coordinator WHERE `coordiantor_name`='$cod_name'");
+                      $rows=mysqli_num_rows($query1);
+                      if($rows>0)
+        {
+                        $row=mysqli_fetch_array($query1);
+                        $society_id=$row['society_id'];
+
+$query2="SELECT * FROM `event` WHERE `society_id`='$society_id' AND  `event_status` = 'granted'";
+$query_run2=mysqli_query($connection,$query2);
+}
+?>
+   
+ <label ></label>
+ 
+ <select style="margin-left: 30%; width: 40%" placeholder="Enter Event Name" type="text" name="event">
+
+
+ 
+ 
+  <?php
+if($query_run2==true)
+{
+  
+  {
+    $count=1;
+    
+    if(mysqli_num_rows($query_run2)>=1)
+       {
+              while($row=mysqli_fetch_assoc($query_run2))
+          { ?>
+             <option > 
+             <?php echo $row['event_name']?>
+             </option> 
+
+             <?php
+             $count=$count+1;
+         }
+       }
+  else
+
+      {
+         echo 'no file';
+     }
+  }
+}
+?>
+            <br>
+            <br>  
+             <input style="width: 15%" type="submit" name = "submit" value = "submit" >
+      
       </form>
      <br>
   </div>
@@ -97,8 +149,15 @@ body{
 </div>
 
 <div class="footer">
-  <p>Thank You</p>
+  <p>EMS AKGEC 2017</p>
 </div>
 
 </body>
 </html>
+<?php
+}
+else
+{
+  header('location:index.php');
+}
+?>
